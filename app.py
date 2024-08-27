3c9660fab6a6e03b009a3a136448fca48c017dd7
unknown <68788400+gmprojects-ctrl@users.noreply.github.com>
2024-08-27 17:56:34 +0100

    Added GARCH

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ from pathlib import Path
 import plotly.express as px
 import plotly.graph_objects as go   
 from scipy.stats import norm
+from statsmodels.tsa.stattools import acf, pacf
+import arch
 
 from comm_data import COMMODITY_LIST
 
@@ -117,7 +119,16 @@ def main():
     # Sort the data
     commodity_data = commodity_data.sort_index(ascending=True).copy()
     
-     
+     # Calculate the log returns
+    log_returns = get_log_returns(commodity_data['Close'])
+        
+    # Get a rolling window
+    rolling_window = st.number_input(label="Enter a rolling window",min_value=1,step=1,value=7)
+        
+    # Sum the log returns
+    sum_log_returns = log_returns.rolling(rolling_window).sum().dropna()
+    
+    
     with st.expander("See data"):
         # Write that data
         st.write(commodity_data)
@@ -130,22 +141,10 @@ def main():
         
         
         
-        # Calculate the log returns
-        log_returns = get_log_returns(commodity_data['Close'])
-        
-        # Get a rolling window
-        rolling_window = st.number_input(label="Enter a rolling window",min_value=1,step=1,value=7)
-        
-        # Sum the log returns
-        sum_log_returns = log_returns.rolling(rolling_window).sum().dropna()
+
         
         
-        
-        # Plot the data
-        st.plotly_chart(px.line(sum_log_returns,x=sum_log_returns.index,y='Log Returns',title=f"{commodity} Log Returns over {rolling_window} day rolling sum"))
-        
-        # Plot the histogram
-        st.plotly_chart(px.histogram(sum_log_returns,x='Log Returns',title=f"{commodity} Log Returns Histogram over {rolling_window} day rolling sum", nbins=100))
+      
         
         # Calculate the value at risk
         log_value_at_risk = np.quantile(sum_log_returns['Log Returns'],0.05)
@@ -155,17 +154,43 @@ def main():
         log_expected_shortfall = sum_log_returns[sum_log_returns['Log Returns'] <= log_value_at_risk].mean().values[0]
         expected_shortfall = np.exp(log_expected_shortfall)
         
+        
+          
+        # Plot the data
+        st.plotly_chart(px.line(sum_log_returns,x=sum_log_returns.index,y='Log Returns',title=f"{commodity} Log Returns over {rolling_window} day rolling sum"))
+        
+        # Plot the histogram
+        fig = go.Figure()
+        fig.add_trace(go.Histogram(x=sum_log_returns['Log Returns'],nbinsx=50))
+        fig.add_vline(x=log_value_at_risk,line=dict(color='red'),name="Value at Risk")  
+        fig.add_vline(x=log_expected_shortfall,line=dict(color='green'),name="Expected Shortfall")
+        
+        fig.update_layout(title=f"Histogram of Log Returns for {commodity} over {rolling_window} day period")
+        
+        st.plotly_chart(fig)
+        
+        
         # Write the value at risk
         st.write(f"The value at risk is {(1-value_at_risk)*100:.2f}% at the 5% confidence level over a {rolling_window} day period") 
         st.write(f"The expected shortfall is {(1-expected_shortfall)*100:.2f}% at the 5% confidence level over a {rolling_window} day period")
 
         
+    
+    
+    with st.expander("Options Pricing"):
+        
+        # Calculate the daily volatility
+        daily_vol = log_returns.std().values[0]
+        
         
         # Calculate volatility
-        volatility = log_returns.std().values[0] * np.sqrt(252)
+        annual_vol = daily_vol * np.sqrt(252)
+        
+        # Write the daily volatility of log returns
+        st.write(f"The daily volatility of log returns is {daily_vol:.2f}")
         
         # Write the annualized volatility of log returns
-        st.write(f"The annualized volatility of log returns is {volatility:.2f}")
+        st.write(f"The annualized volatility of log returns is {annual_vol:.2f}")
         
         # Enter a strike price
         strike_price = st.number_input(label="Enter a strike price",min_value=0.0,step=0.01,value=1000.0)
@@ -180,12 +205,146 @@ def main():
         delta_t = time_to_expiration/12
         
         # Calculate the call price
-        call_price = call_black_scholes(commodity_data['Close'].iloc[-1],strike_price,volatility,rate,delta_t)
+        call_price = call_black_scholes(commodity_data['Close'].iloc[-1],strike_price,annual_vol,rate,delta_t)
         
         # Write the call price
         st.write(f"The call price is {call_price:.2f}")
         
+    with st.expander(f"Using GARCH model for {commodity} log returns over {rolling_window} day period"):
+        st.markdown("""
+        ## GARCH Model
+        
+        Consider the following model:
+        $$y_t = \mu + \epsilon_t$$
+        
+        Where $y_t$ is the log returns, $\mu$ is the mean and $\epsilon_t = \sigma_t * z_t$ where $z_t$ is $N(0,1)£.
+        
+        Where $y_t$ is the log returns, $\mu$ is the mean, $\sigma_t$ is the volatility and $z_t$ is N(0,1).
+        
+        Then the GARCH(p,q) model is given by:
+        """)
+        
+        st.markdown(r'$$\sigma_t^2 = \omega + \sum_{i=1}^{p} \alpha_i \epsilon_{t-i}^2 + \sum_{i=1}^{q} \beta_i \sigma_{t-i}^2$$')
+        
+        
+        # Garch data
+        # Scale by 1000
+        garch_data = 1000*(sum_log_returns)
+        
+        
+        # Create a 80/20 split
+        training_data = garch_data.iloc[:int(len(garch_data)*0.8)].copy()
+        testing_data = garch_data.iloc[int(len(garch_data)*0.8):].copy()
+        conditional_training_data = training_data.copy()    
+        
+        # Divider
+        st.divider()
+        
+        # Note the log returns are mulitplied by a 1000
+        st.markdown("Note the log returns are mulitplied by a 1000")
+        
+        # Create tabs
+        tabs = st.tabs(["Training Data","Testing Data"])
+        
+        # Show the training data
+        with tabs[0]:
+            st.write(training_data)
+        
+        # Show the testing data
+        with tabs[1]:
+            st.write(testing_data)
+            
+        # PACF plot 
+        pacf_plot = pacf(training_data['Log Returns']**2,nlags=20)
+        
+        # ACF plot
+        acf_plot = acf(training_data['Log Returns']**2,nlags=20)
+        
+        # Create a figure
+        fig = go.Figure()
+        # Add the PACF plot
+        fig.add_trace(go.Bar(x=np.arange(len(pacf_plot)),y=pacf_plot,name="PACF"))
+        
+        # Add the ACF plot
+        fig.add_trace(go.Bar(x=np.arange(len(acf_plot)),y=acf_plot,name="ACF"))
+        
+        # Set the xaxis
+        fig.update_xaxes(title="Lag Period")
+        
+        # Set the yaxis
+        fig.update_yaxes(title="Correlation")
+        
+        # Update the layout
+        fig.update_layout(title="PACF and ACF plot for GARCH model (note the data is squared as it is sigma^2)")
+        
+        
+        # Plot the figure
+        st.plotly_chart(fig)
+        
+        
+        # P,Q input
+        p_garch = st.number_input(label="Enter a p value for the GARCH model",min_value=0,step=1,value=1)
+        q_garch = st.number_input(label="Enter a q value for the GARCH model",min_value=0,step=1,value=1)
 
+        # Create an Arch Model
+        model = arch.arch_model(training_data['Log Returns'],mean='Zero',vol='Garch',p=p_garch,q=q_garch)
+        
+        # Fit the model
+        model_fit = model.fit()
+        
+        # Get the conditional volatility (which is the square root of the variance and equal to the |log returns|)
+        conditional_volatility = model_fit.conditional_volatility
+        
+        # Plot the conditional volatility
+        fig = go.Figure()
+        fig.add_trace(go.Scatter(x=training_data.index,y=conditional_volatility,name="Conditional Volatility", line=dict(color='red')))
+        fig.add_trace(go.Scatter(x=training_data.index,y=np.abs(training_data['Log Returns']),name="Log Returns"))
+        fig.update_layout(title=f"Model Fit for GARCH({p_garch,q_garch}) for {commodity} over {rolling_window} day period on Training Data")
+        st.plotly_chart(fig)
+        
+        # Get the forecast
+        forcasted_values = pd.DataFrame(index=testing_data.index,columns=['Forecasted Volatility'])
+        
+        for i in range(len(testing_data)):
+            # Get prediction
+            prediction = model_fit.forecast(horizon=1).variance.iloc[0].values[0]
+            
+            # Forecast
+            forcasted_values.iloc[i] = np.sqrt(prediction)
+            
+            # Update the model
+            conditional_training_data = pd.concat([conditional_training_data,testing_data.iloc[i:i+1,:]])
+            
+            # Update the model
+            model = arch.arch_model(conditional_training_data['Log Returns'],mean='Zero',vol='Garch',p=p_garch,q=q_garch)
+            
+            # Fit the model
+            model_fit = model.fit()
+            
+        
+            
+        # Plot the forecasted values
+        fig = go.Figure()
+        fig.add_trace(go.Scatter(x=testing_data.index,y=forcasted_values['Forecasted Volatility'],name="Forecasted Volatility",line=dict(color='red')))
+        fig.add_trace(go.Scatter(x=testing_data.index,y=np.abs(testing_data['Log Returns']),name="Log Returns (absolute value)"))
+        fig.update_layout(title=f"Rolling Forecasted Volatility ({p_garch,q_garch}) for {commodity} over {rolling_window} day period on Testing Data")
+        st.plotly_chart(fig)
+        
+        # Root mean squared error
+        rmse = np.sqrt(np.mean((forcasted_values['Forecasted Volatility'] - np.abs(testing_data['Log Returns']))**2))
+        
+        
+        
+        # Write the RMSE
+        st.write(f"The RMSE is {rmse:.2f}")
+        
+        # Write the model summary
+        st.write(model_fit.summary())
+        
+        # Write the model parameters
+        st.write(model_fit.params)
+        
+        
 
 # Run the function
 if __name__ == "__main__":
